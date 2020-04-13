bd80fed45805f3c05c0bf69e9ac2bdc9e383dcf3
Alberto <Alberto@socialpymesvlc.es>
2020-04-13 19:05:51 +0200

    full connection successfully

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
-import * as React from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, SafeAreaView, View} from 'react-native';
 
 export default function App() {
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rithmi-frontend-test.s3-eu-west-1.amazonaws.com/samples.json')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    });
+
     return (
-        <View style={styles.container}>
-            <Text>eto es una prueba</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+          {
+            data.map((item, index)=>{
+              return (
+                  <Text>Numero: { index } => {item.date}</Text>
+              )
+            })
+          }
+            <Text>esto es una prueba</Text>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
 });
